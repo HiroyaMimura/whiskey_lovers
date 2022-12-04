bf686fc947755a741ac5f61d0292c4df6fee1179
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-04 03:31:13 +0000

    gem導入

--- Gemfile
+++ Gemfile
@@ -54,3 +54,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
+gem 'rails-i18n'
+gem 'devise-i18n-views'
+gem 'kaminari','~> 1.2.1'
+gem 'bootstrap5-kaminari-views', '~> 0.0.1'
+gem 'dotenv-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" 
+import '@fortawesome/fontawesome-free/js/all'
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
+
 
 Rails.start()
 Turbolinks.start()
