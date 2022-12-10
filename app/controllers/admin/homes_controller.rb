d543a6d82e6dd6fa1d85d396b4a85a3badf5fc6c
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-10 01:33:33 +0000

    ヘッダー、フッター、トップ画面レイアウト調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,7 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @whiskeys = Whiskey.page(params[:page]).per(5)
+    @whiskeys = Whiskey.page(params[:page]).per(2)
     @tag_list = Tag.all
   end
 end
