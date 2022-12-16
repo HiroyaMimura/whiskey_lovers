53c6849517882697e672fb032c1864231c8f7c3c
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-16 03:14:54 +0000

    migrateファイル修正

--- db/migrate/20221204040438_create_whiskey_comments.rb
+++ db/migrate/20221204040438_create_whiskey_comments.rb
@@ -1,7 +1,7 @@
 class CreateWhiskeyComments < ActiveRecord::Migration[6.1]
   def change
     create_table :whiskey_comments do |t|
-      
+
       t.integer :user_id, null: false
       t.integer :whiskey_id, null: false
       t.text :comment, null: false
--- db/migrate/20221204041411_create_whiskey_tags.rb
+++ db/migrate/20221204041411_create_whiskey_tags.rb
@@ -2,8 +2,8 @@ class CreateWhiskeyTags < ActiveRecord::Migration[6.1]
   def change
     create_table :whiskey_tags do |t|
 
-      t.references :whiskey_id, null: false, foreign_key: true
-      t.references :tag_id, null: false, foreign_key: true
+      t.references :whiskey, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
       t.timestamps
     end
     # 複合キーインデックスで、この記述で同じタグを2回保存できないようにする
