2dfaf0e6b071012970d268e17763d2d6913375ff
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-18 08:55:03 +0000

    問い合わせ機能修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "whiskey_lovers_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV["GMAIL_ADD"],
+    password: ENV["GMAIL_KEY"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
