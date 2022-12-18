c3da038de18fd4bfbfb7aa2d0ea9efb80679389a
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-18 08:10:30 +0000

    問い合わせ機能修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV["GMAIL_ADD"],
+    password: ENV["GMAIL_KEY"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
@@ -76,16 +87,4 @@ Rails.application.configure do
 
   config.hosts.clear
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: ENV["GMAIL_ADD"],
-    password: ENV["GMAIL_KEY"],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
