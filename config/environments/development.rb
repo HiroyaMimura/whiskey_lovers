d05a6e7ba831fa7e231196238713b334b61c121e
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-18 08:30:27 +0000

    問い合わせ機能修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = { protocol: 'https', host:'https://baseballapp514.herokuapp.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
