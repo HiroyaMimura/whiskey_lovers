7897eac57c52eadebe77a9a7d77f83be7129b453
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-20 09:48:45 +0000

    フラッシュメッセージ修正

--- app/javascript/whiskey_lovers.js
+++ app/javascript/whiskey_lovers.js
@@ -1,8 +1,9 @@
 $(document).on('turbolinks:load',function(){
   $('.top_image').fadeIn(3500, function(){
   $('.message').fadeIn(4000, function(){
-  $('.message2').fadeIn(4000);
-        });
+  $('.message2').fadeIn(4000, function(){
+  $('.logout').slideUp(2000)
       });
     });
-
+  });
+});
