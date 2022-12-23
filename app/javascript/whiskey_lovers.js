f60e6c56da73d2cb18db84f7db79a39a85f4ade0
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-12-23 02:25:58 +0000

    レイアウト調整

--- app/javascript/whiskey_lovers.js
+++ app/javascript/whiskey_lovers.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function(){
   $('.top_image').fadeIn(3500, function(){
-  $('.message').fadeIn(4000, function(){
-  $('.message2').fadeIn(4000, function(){
-  $('.logout').slideUp(2000)
+    $('.message').fadeIn(4000, function(){
+      $('.message2').fadeIn(4000, function(){
+        $('.logout').slideUp(2000)
       });
     });
   });
