be155eb5845621c5556cfd5f590207494788e876
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-15 05:49:26 +0000

    TOP画面調整

--- app/javascript/whiskey_lovers.js
+++ app/javascript/whiskey_lovers.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function(){
   $('.top_image').fadeIn(3000, function(){
-    $('.message').fadeIn(3500, function(){
-      $('.message2').fadeIn(3500, function(){
+    $('.message').fadeIn(3000, function(){
+      $('.message2').fadeIn(3000, function(){
         $('.logout').slideUp(2000)
       });
     });
